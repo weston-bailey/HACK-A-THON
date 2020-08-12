c1c8307dfc3b1d6df5c03fd14ede70428045c280
Emily Krauss <e.krauss27@gmail.com>
2020-08-12 16:35:01 -0700

    fixed weather component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 // import { BrowserRouter, Route, Link } from "react-router-dom";
 import Weather from './components/Weather.jsx'
+import Video from './components/Video.jsx'
 
 function App() {
   return (
     <div className="App">
 
-      <Weather />
     </div>
   );
 }
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,28 +1,9 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+
 import Image from 'react-bootstrap/Image';
 
 export default function Weather() {
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <Image src="/weather-main.png" alt="meteor weather" />
-                </Col>
-            </Row>
-            <Row >
-                <Col>
-                    <Image src="/weather-wed.png" alt="wednesday weather" />
-                </Col>
-                <Col>
-                    <Image src="/weather-thurs.png" alt="wednesday weather" />
-                </Col>
-                <Col>
-                    <Image src="/weather-fri.png" alt="wednesday weather" />
-                </Col>
-            </Row>
-        </Container>     
+        <Image src="/weather.png" height="200" width="200" />
     )
 }
