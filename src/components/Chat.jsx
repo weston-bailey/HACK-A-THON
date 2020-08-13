d4c237035fbc9cc7a885195d11e1a9d707750ab0
Weston Bailey <void@MBP.local>
2020-08-13 10:24:52 -0700

    add chat styles

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,14 +1,14 @@
 import React, { useState} from 'react';
 import './chat.css'
-import {Button, Modal, Form, FormControl} from 'react-bootstrap';
+import {Button, Modal, Form, FormControl, Image, Container, Row} from 'react-bootstrap';
 
 const chatTexts = [
-  "Where was the Kaboom? There was supposed to be an earth-shattering kabaoom!",
-  "Have you tried restarting it?",
-  "Sometimes you just have to keep smashing until they fit.",
   "When humans smack each other's hands it is a sign of respect, when they smack each other's faces it is an insult. Don’t mix it up.",
+  "Sometimes you just have to keep smashing until they fit.",
+  "Have you tried restarting it?",
+  "It is no longer polite to smash a glass on the ground as a sign of appreciation for the drink.",
+  "Where was the Kaboom? There was supposed to be an earth-shattering kabaoom!",
   "Turn the third knob three degrees to the right and hit ignite.",
-  "It is no longer polite to smash a glass on the ground as a sign of appreciation for the drink."
 ]
 
 const Chat = () => {
@@ -48,18 +48,27 @@ const Chat = () => {
         centered='true'
       >
         <Modal.Header closeButton className="orangeBg">
-          <Modal.Title>Ask Zork</Modal.Title>
+          <Modal.Title className="whiteText">Ask Zork</Modal.Title>
         </Modal.Header>
 
         <Modal.Body className="orangeBg">
-
+          <Container>
+          <Row className="justify-content-md-center">
+            <Image src="./Zorkhead.png" width="75vw"/> 
+          </Row>
+          <Row className="justify-content-md-center">
+           <p className="whiteText">
+            Our award winning advice columnist is here to answer all of your most important questions; maners, relationship advice, and understanding the complexity of human cultures.
+           </p>
+          </Row>
+          </Container>
           <Form>
             <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Zork Says</Form.Label>
+              <Form.Label className="whiteText">Zork Says:</Form.Label>
               <Form.Control as="textarea" rows="3" value={zorkText} />
             </Form.Group>
             <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Talk to Zork</Form.Label>
+              <Form.Label className="whiteText">Talk to Zork:</Form.Label>
               <Form.Control as="textarea" rows="3" value={inputText} onChange={(e) => handleInput(e)}/>
             </Form.Group>
           </Form>
