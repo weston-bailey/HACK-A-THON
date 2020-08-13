ceb671eea6ebe3dfdaf0ec30f12c4dd957432009
Emily Krauss <e.krauss27@gmail.com>
2020-08-13 10:25:32 -0700

    fixed left rail of home page

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 
 export default function Video() {
     return (
-        <>
-        <Image src="/video-aliens.png" width="200" height="100" /> 
-        <p></p>
-        <Image src="/video-storms.png" width="200" height="100"/>
-        </>
+        <Container>
+            <Row>
+                <h3>Videos</h3>
+            </Row>
+            <Row>
+                <Image src="/videos.png" width="100%" rounded fluid />
+            </Row>
+        </Container>
     )   
 }
